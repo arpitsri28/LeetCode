bb76a1d22f071dcc9cc67cf65ef4cea1a3d9a0fa
arpitsri28 <156077771+arpitsri28@users.noreply.github.com>
2025-01-15 22:42:48 -0500

    Time: 24 ms (56.77%) | Memory: 26.1 MB (26.16%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,17 +3,17 @@ class Solution:
         n = len(nums)
         prefix = [1] * n
         suffix = [1] * n
-        product = 1
-        for i in range(n):
-            prefix[i] = product
-            product = product * nums[i]
-
+        product_p = 1
         j = len(nums) - 1
-        product = 1
-        while j >= 0:
-            suffix[j] = product
-            product = product * nums[j]
+        product_s = 1
+        i = 0
+        while j >= 0 and i < n:
+            suffix[j] = product_s
+            product_s = product_s * nums[j]
+            prefix[i] = product_p
+            product_p = product_p * nums[i]
             j = j - 1
+            i = i + 1
 
         results = [1] * n
         for k in range(n):
