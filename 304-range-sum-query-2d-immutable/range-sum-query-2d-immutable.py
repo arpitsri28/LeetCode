7fc98469938a36c856470f51a0fb769a36fa1a97
Arpit Srivastav <156077771+arpitsri28@users.noreply.github.com>
2025-04-09 20:04:55 -0400

    Time: 111 ms (64.03%) | Memory: 30.8 MB (65.72%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,32 +1,27 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.prefix_matrix = []
         rows = len(matrix)
         cols = len(matrix[0])
+        self.prefix_matrix = [[0] * cols for _ in range(rows)]
         i = 0
         j = 0
         for i in range(rows):
             total = 0
-            prefix = []
             for j in range(cols):
                 total += matrix[i][j]
-                prefix.append(total)
-            self.prefix_matrix.append(prefix)
+                self.prefix_matrix[i][j] = total
+                if i > 0:
+                    self.prefix_matrix[i][j] += self.prefix_matrix[i-1][j]
 
 
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        n = row1
-        i = 0
-        total = 0
-        while n <= row2:
-            preRight = self.prefix_matrix[n][col2]
-            preLeft = self.prefix_matrix[n][col1 - 1] if col1 > 0 else 0
-            total += preRight - preLeft
-            n += 1
-        return total
-
+        total = self.prefix_matrix[row2][col2]
+        left = self.prefix_matrix[row2][col1 - 1] if col1 > 0 else 0
+        above = self.prefix_matrix[row1 - 1][col2] if row1 > 0 else 0
+        common = self.prefix_matrix[row1 - 1][col1 - 1] if row1 > 0 and col1 > 0 else 0
+        return (total - left - above + common)
         
 
 
