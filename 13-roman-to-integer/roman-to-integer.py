0220c4529467e1ce587a16dac47d54fcb5fccfe3
Arpit Srivastav <156077771+arpitsri28@users.noreply.github.com>
2025-10-06 23:44:18 -0400

    Time: 6 ms (46.57%) | Memory: 17.6 MB (90.24%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,58 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        hashInt = {}
+        hashInt['V'] = 5
+        hashInt['L'] = 50
+        hashInt['D'] = 500
+        hashInt['M'] = 1000
+        total = 0
+        i = 0
+        while i < len(s):
+            if s[i] == 'I':
+                if i != len(s) - 1:
+                    if s[i + 1] == 'V':
+                        total += 4
+                        i += 1
+                    elif s[i + 1] == 'X':
+                        total += 9
+                        i += 1
+                    else:
+                        total += 1
+                else:
+                    total += 1
+            elif s[i] == 'X':
+                if i != len(s) - 1:
+                    if s[i + 1] == 'L':
+                        total += 40
+                        i += 1
+                    elif s[i + 1] == 'C':
+                        total += 90
+                        i += 1
+                    else:
+                        total += 10
+                else:
+                    total += 10
+            elif s[i] == 'C':
+                if i != len(s) - 1:
+                    if s[i + 1] == 'D':
+                        total += 400
+                        i += 1
+                    elif s[i + 1] == 'M':
+                        total += 900
+                        i += 1
+                    else:
+                        total += 100
+                else:
+                    total += 100
+            else:
+                total += hashInt[s[i]]
+            
+            i += 1
+        
+        return total
+            
+
+                    
+
+
+        
