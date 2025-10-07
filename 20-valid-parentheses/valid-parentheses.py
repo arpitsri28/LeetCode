a11a8fc044d41b1f009ccc03adac406a0aeb1fb1
Arpit Srivastav <156077771+arpitsri28@users.noreply.github.com>
2025-10-07 00:01:10 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (14.21%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        hashBracket = {'(':')', '{':'}', '[':']'}
+
+        for i in range(len(s)):
+            if s[i] in hashBracket:
+                stack.append(s[i])
+            else:
+
+                if not stack or s[i] != hashBracket[stack[-1]]:
+                    return False
+                stack.pop()
+
+        
+        if len(stack) != 0:
+            return False
+
+        return True
+
+
+        
